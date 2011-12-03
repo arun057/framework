86f961e0bf9f52331247ec909af48bb13fb02c8e
Arunram Kalaiselvan <arun@edengroups.com>
2011-12-02 20:58:25 -0500

    Wrapping up page.php

--- application/modules/cms/views/page.php
+++ application/modules/cms/views/page.php
@@ -21,7 +21,7 @@
 			} ?>
 		</aside>
 		<section id="main" class="grid_6">
-			<article id="<?php echo $page_title; ?>">
+			<article class="content">
 				<h1><?php echo $page_title; ?></h1>
 				<?php
 				$attributes = array('id' => 'my_form', 'name' => 'my_form');
@@ -40,13 +40,13 @@
 					<div id='admin_title'>
 						<?php
 							echo form_label('Title:', 'title');
-							echo form_input('title', $page->title);
+							echo form_input('title', $page->title, 'size="50"');
 						?>
 					</div>
 				</div>
 				<?php
 				echo form_label('Page Content:', 'content');
-				echo form_textarea('content', $page->content,'id="wysiwyg"');
+				echo form_textarea('content', $page->content,'id="wysiwyg" style="width: 400px;"');
 				//echo display_ckeditor($ckeditor);
 				?>
 
@@ -107,170 +107,168 @@
 			</article>
 		</section>
 		<aside class ="grid_3">
-				<div class="sidebar">
-					<div class="box">
-						<div class="status clear">
-							<h3>Status:
-								<?php
-									echo form_dropdown('status', $this->node_statuses, $page->status);
-								?>
-							</h3>
-						</div>
-						
-						<div class="status">
-							<h3>Page Type:
-								<?php
-									echo form_dropdown('menu', $page_type_options, $page->menu);
-								?>
-							</h3>
-							<h3>New Page Type:</h3>
-							<?php
-								echo form_input('new_page_type', '');
-							?>
-						</div>
-
-						<div class="status clear">
-							<h3>
-								<?php
-									echo form_checkbox('allow_comments', $page->allow_comments, $page->allow_comments);
-									echo '&nbsp;Allow Comments:';
-								?>
-							</h3>
-						</div>
-
-						<div class="status clear cms_categorys" >
-							<h3 style="color: white">Categories:</h3>
-							<div style="height: 200px; overflow: auto">
-								<?php 
-									include_once('includes/category_helper.php');
-									list_categories($all_categories, 1, 0, $all_categories, $page->categories); 
-								?>
-							</div>
-						</div>
-						
+			<div class="box">
+				<h3>Status:</h3>
+				<b>Status: </b>
+					<?php
+					echo form_dropdown('status', $this->node_statuses, $page->status);
+					?>
+			</div>
+			<div class="box">
+				<h3>Page Type</h3>
+				<div class="status">
+					<h3>Page Type:
 						<?php
-							if ((isset($sidebar_blocks)) && (!isset($hide_block['Page_Sidebar_Blocks']))) {
-								?>
-								<div id="sidebar_blocks" class="status">
-									<h3>
-										Sidebar: <span style="font-size: 9px">(drag&drop to reorder)</span>
-											<ul style="list-style-type: none; padding: 0">
-												<?php $s_value = '';
-												foreach ($sidebar_blocks as $type => $info) { 
-													$s_value .= ($s_value) ? '&' : '';
-													$s_value .= 'sidebar_block[]=' . $type;
-													?>
-													<li id="sidebar_block_<?= $type ?>">
-														<input type="checkbox" name="sidebar_block_<?= $type ?>" <?= ($info[2]) ? 'checked' : '' ?> ><?= $info[1] 
-														?>
-													</li>
-													<?php 
-												} ?>
-											</ul>
-										<input id="sidebar_blocks_sorted" name="sidebar_blocks_sorted" type="hidden" value="<?= $s_value ?>" />
-									</h3>
-									<a target="_newblock" href="/cms/block/sidebar">Edit sidebar blocks</a>
-								</div>
-								<?php 
-							}
-							
-							if ((isset($page_blocks)) && (!isset($hide_block['Page_Blocks']))) { ?>
-								<div id="page_blocks" class="status">
-									<h3>Blocks: <span style="font-size: 9px">(drag&drop to reorder)</span>
-										<ul style="list-style-type: none; padding: 0">
-											<?php 
-											$s_value = '';
-											foreach ($page_blocks as $type => $info) { 
-												$s_value .= ($s_value) ? '&' : '';
-												$s_value .= 'page_block[]=' . $type;
-												?>
-												<li id="page_block_<?= $type ?>">
-													<input type="checkbox" name="page_block_<?= $type ?>" <?= ($info[2]) ? 'checked' : '' ?> ><?= $info[1] ?>
-												</li>
-												<?php 
-											} ?>
-										</ul>        
-										<input id="page_blocks_sorted" name="page_blocks_sorted" type="hidden" value="<?= $s_value ?>" />
-									</h3>
-									<a target="_newblock" href="/cms/block/page">Edit page blocks</a>
-								</div>
-								<?php 
-							} 
+							echo form_dropdown('menu', $page_type_options, $page->menu);
+						?>
+					</h3>
+					<h3>New Page Type:</h3>
+					<?php
+						echo form_input('new_page_type', '');
+					?>
+				</div>
 
-							if (!isset($hide_block['Page_Video'])) { ?>
-								<div class="status clear"><h3>Video:</h3>
-									<div class=""> <?php echo form_input('video', $page->video); ?></div>
-								</div>
-								<?php 
-							}
+				<div class="status clear">
+					<h3>
+						<?php
+							echo form_checkbox('allow_comments', $page->allow_comments, $page->allow_comments);
+							echo '&nbsp;Allow Comments:';
+						?>
+					</h3>
+				</div>
 
-							if (!isset($hide_block['Page_Article'])) {?>
-								<div class="status clear">
-									<h3>Article Source:</h3>
-									<div class=""> 
-										<?php echo form_input('content_source', $page->content_source); ?>
-									</div>
-								</div>
-								<?php 
-							} ?>
-
-							<div class="status clear">
-								<h3>Monthly Theme Year:</h3>
-								<div class=""> 
-									<?php echo form_input('monthly_theme_year', $page->monthly_theme_year); ?>
-								</div>
-								
-								<h3>Monthly Theme Month:</h3>
-								<div class=""> 
-									<?php echo form_input('monthly_theme_month', $page->monthly_theme_month); ?>
-								</div>
-							</div>
-
-							<div class="status clear">
-							<h3>URL:</h3>
-							<?php 
-							$page_type = preg_replace('/[^0-9a-z ]+/i', '', $page->menu); 
-							$page_type = str_replace(' ', '-', strtolower($page_type));
-
-							$page_url = '/page/' . (($page_type) ? $page_type . '/' : '');  
-
-							if ($page->status != 'publish') { ?>
-								<a href="/page/preview/<?= $page->name ?>" target="_preview" 
-								title="preview: <?= $page_url . $page->name ?>" alt="preview: <?= $page_url . $page->name ?>">preview</a>
-								<br /><br />
-								<span style="font-size: 0.7em;float:left;" ><?= $page_url ?></span>
-								<input type="text" name="name" value="<?= $page->name ?>" 
-								style="float: left; border: none; padding: 2px; padding-left: 0px; width: 150px;" />
-								<?php 
-							} else 
-								echo '<a target="_preview" href="' . $page_url . $page->name . '"><span style="float: left;">' . $page_url . $page->name . '</a>';
-							
-							$sites = $this->config->item( 'sites' );
-							if( is_array( $sites )) {
-								$sites = array_unique( array_values( $sites ));
-							} else {
-								$sites = array();
-							}
-
-							foreach( $sites as $siteId ) {
-								$siteDef = $this->config->item( $siteId );
-								if( is_array( $siteDef )) {
-									$siteURL = $siteDef[ 'base_url' ];
-									echo "<br/><a style='clear: both; float: left;' href=\"$siteURL/page/preview/{$page->name}\" target=\"_preview\">$siteId</a>";
-								}
-							}
-							?>
-							<div class='clear'></div>
-						</div>
+				<div class="status clear cms_categorys" >
+					<h3 style="color: white">Categories:</h3>
+					<div style="height: 200px; overflow: auto">
+						<?php 
+							include_once('includes/category_helper.php');
+							list_categories($all_categories, 1, 0, $all_categories, $page->categories); 
+						?>
 					</div>
 				</div>
-			</aside>
+			</div>
+			<?php
+				if ((isset($sidebar_blocks)) && (!isset($hide_block['Page_Sidebar_Blocks']))) {
+					?>
+					<div id="sidebar_blocks" class="box">
+						<h3>
+							Sidebar: <span style="font-size: 9px">(drag&drop to reorder)</span>
+								<ul style="list-style-type: none; padding: 0">
+									<?php $s_value = '';
+									foreach ($sidebar_blocks as $type => $info) { 
+										$s_value .= ($s_value) ? '&' : '';
+										$s_value .= 'sidebar_block[]=' . $type;
+										?>
+										<li id="sidebar_block_<?= $type ?>">
+											<input type="checkbox" name="sidebar_block_<?= $type ?>" <?= ($info[2]) ? 'checked' : '' ?> ><?= $info[1] 
+											?>
+										</li>
+										<?php 
+									} ?>
+								</ul>
+							<input id="sidebar_blocks_sorted" name="sidebar_blocks_sorted" type="hidden" value="<?= $s_value ?>" />
+						</h3>
+						<a target="_newblock" href="/cms/block/sidebar">Edit sidebar blocks</a>
+					</div>
+					<?php 
+				}
+				
+				if ((isset($page_blocks)) && (!isset($hide_block['Page_Blocks']))) { ?>
+					<div  class="box menu">
+						<h3>Blocks: <span style="font-size: 9px">(drag&drop to reorder)</span>
+							<ul style="list-style-type: none; padding: 0">
+								<?php 
+								$s_value = '';
+								foreach ($page_blocks as $type => $info) { 
+									$s_value .= ($s_value) ? '&' : '';
+									$s_value .= 'page_block[]=' . $type;
+									?>
+									<li id="page_block_<?= $type ?>">
+										<input type="checkbox" name="page_block_<?= $type ?>" <?= ($info[2]) ? 'checked' : '' ?> ><?= $info[1] ?>
+									</li>
+									<?php 
+								} ?>
+							</ul>        
+							<input id="page_blocks_sorted" name="page_blocks_sorted" type="hidden" value="<?= $s_value ?>" />
+						</h3>
+						<a target="_newblock" href="/cms/block/page">Edit page blocks</a>
+					</div>
+					<?php 
+				} 
+
+				if (!isset($hide_block['Page_Video'])) { ?>
+					<div class="box"><h3>Video:</h3>
+						<div class=""> <?php echo form_input('video', $page->video); ?></div>
+					</div>
+					<?php 
+				}
+
+				if (!isset($hide_block['Page_Article'])) {?>
+					<div class="box">
+						<h3>Article Source:</h3>
+						<div class=""> 
+							<?php echo form_input('content_source', $page->content_source); ?>
+						</div>
+					</div>
+					<?php 
+				} ?>
+
+				<div class="box info">
+					<h3>Monthly Theme Year:</h3>
+					<div class=""> 
+						<?php echo form_input('monthly_theme_year', $page->monthly_theme_year); ?>
+					</div>
+					
+					<h3>Monthly Theme Month:</h3>
+					<div class=""> 
+						<?php echo form_input('monthly_theme_month', $page->monthly_theme_month); ?>
+					</div>
+				</div>
+
+				<div class="box">
+				<h3>URL:</h3>
+				<?php 
+				$page_type = preg_replace('/[^0-9a-z ]+/i', '', $page->menu); 
+				$page_type = str_replace(' ', '-', strtolower($page_type));
+
+				$page_url = '/page/' . (($page_type) ? $page_type . '/' : '');  
+
+				if ($page->status != 'publish') { ?>
+					<a href="/page/preview/<?= $page->name ?>" target="_preview" 
+					title="preview: <?= $page_url . $page->name ?>" alt="preview: <?= $page_url . $page->name ?>">preview</a>
+					<br /><br />
+					<span style="font-size: 0.7em;float:left;" ><?= $page_url ?></span>
+					<input type="text" name="name" value="<?= $page->name ?>" 
+					style="float: left; border: none; padding: 2px; padding-left: 0px; width: 150px;" />
+					<?php 
+				} else 
+					echo '<a target="_preview" href="' . $page_url . $page->name . '"><span style="float: left;">' . $page_url . $page->name . '</a>';
+				
+				$sites = $this->config->item( 'sites' );
+				if( is_array( $sites )) {
+					$sites = array_unique( array_values( $sites ));
+				} else {
+					$sites = array();
+				}
+
+				foreach( $sites as $siteId ) {
+					$siteDef = $this->config->item( $siteId );
+					if( is_array( $siteDef )) {
+						$siteURL = $siteDef[ 'base_url' ];
+						echo "<br/><a style='clear: both; float: left;' href=\"$siteURL/page/preview/{$page->name}\" target=\"_preview\">$siteId</a>";
+					}
+				}
+				?>
+				<div class='clear'></div>
+			</div>
+	
 			<?php
 			$data = array('name' => 'submit','value' => 'Save changes','class' => 'button');
 			echo form_submit($data);
 			echo form_close();
-			?>
-
+			?>	
+		</aside>
+		
 <script type="text/javascript">
 
 $(document).ready(function() { 	
